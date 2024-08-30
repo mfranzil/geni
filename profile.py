a1cd25660eb9a02e6b1202c365fafce040e5f8c3
Matteo Franzil <matteofranzil@gmail.com>
2024-08-30 14:22:00 +0200

    aaaaaa

--- profile.py
+++ profile.py
@@ -1,19 +1,37 @@
 """
-A profile with a configurable number of VMs, each with 4 cores and 4GB of RAM, and a setup script that installs a public key for each VM and PostgreSQL.
+A profile with a configurable number of nodes that runs a Postgres database on each node.
 
 Instructions:
-1. Run the profile with the desired number of VMs.
-3. Make changes to the setup.sh script as needed.
-4. Run the profile.
+1. Run the profile.
 """
 
 import geni.portal as portal
 import geni.rspec.pg as rspec
 
+imageList = [
+    ('default', 'Default Image'),
+    ('urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU22-64-STD', 'UBUNTU 22.04'),
+    ('urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU20-64-STD', 'UBUNTU 20.04'),
+    ('urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU18-64-STD', 'UBUNTU 18.04'),
+    ('urn:publicid:IDN+emulab.net+image+emulab-ops//CENTOS8S-64-STD',  'CENTOS 8 Stream'),
+    ('urn:publicid:IDN+emulab.net+image+emulab-ops//FBSD123-64-STD', 'FreeBSD 12.3'),
+    ('urn:publicid:IDN+emulab.net+image+emulab-ops//FBSD131-64-STD', 'FreeBSD 13.1')]
+
+pc.defineParameter("osImage", "Select OS image",
+                   portal.ParameterType.IMAGE,
+                   imageList[0], imageList,
+                   longDescription="Most clusters have this set of images, " +
+                   "pick your favorite one.")
+
+# Optional physical type for all nodes.
+pc.defineParameter("phystype",  "Optional physical node type",
+                   portal.ParameterType.NODETYPE, "d820",
+                   longDescription="Pick a single physical node type (pc3000,d710,etc) " +
+                   "instead of letting the resource mapper choose for you.")
+
 # Describe the parameter(s) this profile script can accept.
-portal.context.defineParameter("n", "Number of nodes", portal.ParameterType.INTEGER, 1) 
-portal.context.defineParameter("hw", "Hardware Type", portal.ParameterType.STRING, "d820")
-portal.context.defineParameter("os", "Operating System", portal.ParameterType.IMAGE, "urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU22-64-STD")
+portal.context.defineParameter("n", "Number of nodes", portal.ParameterType.INTEGER, 1,
+                                 longDescription="Choose the number of nodes to allocate. 1-8 nodes are allowed.")
 
 # Retrieve the values the user specifies during instantiation.
 params = portal.context.bindParameters()
@@ -31,8 +49,8 @@ portal.context.verifyParameters()
 for i in range(params.n):
     # Create a XenVM and add it to the RSpec.
     node = request.RawPC("node" + str(i))
-    node.hardware_type = params.hw
-    node.disk_image = params.os
+    node.hardware_type = params.phystype
+    node.disk_image = params.osImage
     # node = request.XenVM("node_" + str(i))
     # node.cores = 4
     # node.ram = 4096
